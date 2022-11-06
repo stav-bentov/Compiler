e9d648587f9df0ab828ce1f5d9e889d9f79ba415
Rotem Assa <rotem_a@optimove.com>
2022-11-06 15:05:09 +0200

    added token names to enum

--- ex1/src/TokenNames.java
+++ ex1/src/TokenNames.java
@@ -1,12 +1,34 @@
 public interface TokenNames {
   /* terminals */
-  public static final int EOF = 0;
-  public static final int PLUS = 1;
-  public static final int MINUS = 2;
-  public static final int TIMES = 3;
-  public static final int DIVIDE = 4;
-  public static final int LPAREN = 5;
-  public static final int RPAREN = 6;
-  public static final int NUMBER = 7;
-  public static final int ID = 8;
+  public static final int LPAREN = 1;
+  public static final int RPAREN = 2;
+  public static final int LBRACK = 3;
+  public static final int RBRACK = 4;
+  public static final int LBRACE = 5;
+  public static final int RBRACE = 6;
+  public static final int NIL = 7;
+  public static final int PLUS = 8;
+  public static final int MINUS = 9;
+  public static final int TIMES = 10;
+  public static final int DIVIDE = 11;
+  public static final int COMMA = 12;
+  public static final int DOT = 13;
+  public static final int SEMICOLON = 14;
+  public static final int TYPE_INT = 15;
+  public static final int TYPE_STRING = 16;
+  public static final int TYPE_VOID = 17;
+  public static final int ASSIGN = 18;
+  public static final int EQ = 19;
+  public static final int LT = 20;
+  public static final int GT = 21;
+  public static final int ARRAY = 22;
+  public static final int CLASS = 23;
+  public static final int EXTENDS = 24;
+  public static final int RETURN = 25;
+  public static final int WHILE = 26;
+  public static final int IF = 27;
+  public static final int NEW = 28;
+  public static final int INT = 29;
+  public static final int STRING = 30;
+  public static final int ID = 31;
 }
