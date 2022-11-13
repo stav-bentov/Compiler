4d21de9ad1c84cc6f277585cd4598132e52a60ab
stav-bentov <stavbentov5@gmail.com>
2022-11-13 21:19:43 +0200

    Regex Definitions

--- ex1/src/Tests/CreateInputsAndOutputs.java
+++ ex1/src/Tests/CreateInputsAndOutputs.java
@@ -7,9 +7,9 @@ import java.util.List;
 import java.util.Random;
 
 public class CreateInputsAndOutputs {
-    private static String inputsPath = "C:\\Users\\lilac\\Documents\\Compilation\\compilation\\ex1\\input\\";
+    private static String inputsPath = "C:\\Stav\\TAU\\Projects\\Compilation\\compilation\\ex1\\input\\";
     private static String outputsPath = "";
-    private static String ex1PathLinux = "/mnt/c/users/lilac/Documents/Compilation/compilation/ex1";
+    private static String ex1PathLinux = "/mnt/c/Stav/TAU/Projects/Compilation/compilation/ex1";
     private static int numOfStrings = 10;
     private static int numOfInputs = 1;
 
@@ -21,12 +21,51 @@ public class CreateInputsAndOutputs {
     }
 
     private static void CreateInputs() {
-//        deleteFolderContent(new File(inputsPath));
 
-        // TODO: Add generex for each regex
-        generexes.add(new Generex("[\\(\\)\\{\\}\\[\\]+\\-*/;.?!]|[a-zA-Z]|[0-9]"));
+        deleteFolderContent(new File(inputsPath));
+        //********** Regex Definitions ***********/
+        // String
         generexes.add(new Generex("\\\"[a-zA-Z]*\\\""));
+        // ID
         generexes.add(new Generex("[a-zA-Z]+[0-9a-zA-Z]*"));
+        // INT
+        generexes.add(new Generex("[0-9]+"));
+        // Comment type 1
+        generexes.add(new Generex("//[+-*/;.?!a-zA-Z0-9(){}\\[\\]]*[\n\r]"));
+        // Comment type 2
+        // TODO: notice that it can generate "/*____*/___*/" which is NOT valid
+        generexes.add(new Generex("/\\*[+-*/;.?!a-zA-Z0-9(){}\\[\\]\n\r]*\\*/"));
+        // general regexes
+        generexes.add(new Generex("[ \t]"));
+        generexes.add(new Generex("[\r\n]"));
+        generexes.add(new Generex("[(]"));
+        generexes.add(new Generex("[)]"));
+        generexes.add(new Generex("[{]"));
+        generexes.add(new Generex("[}]"));
+        generexes.add(new Generex("[\\[]"));
+        generexes.add(new Generex("[\\]]"));
+        generexes.add(new Generex("nil"));
+        generexes.add(new Generex("+"));
+        generexes.add(new Generex("-"));
+        generexes.add(new Generex("*"));
+        generexes.add(new Generex("/"));
+        generexes.add(new Generex(","));
+        generexes.add(new Generex("\\."));
+        generexes.add(new Generex(";"));
+        generexes.add(new Generex("int"));
+        generexes.add(new Generex("string"));
+        generexes.add(new Generex("void"));
+        generexes.add(new Generex(":="));
+        generexes.add(new Generex("="));
+        generexes.add(new Generex(">"));
+        generexes.add(new Generex("\\<"));
+        generexes.add(new Generex("array"));
+        generexes.add(new Generex("class"));
+        generexes.add(new Generex("extends"));
+        generexes.add(new Generex("return"));
+        generexes.add(new Generex("while"));
+        generexes.add(new Generex("if"));
+        generexes.add(new Generex("new"));
 
         createRandomInputs(numOfInputs);
     }
@@ -69,13 +108,30 @@ public class CreateInputsAndOutputs {
         Random rand = new Random();
         StringBuilder randomInput = new StringBuilder();
 
+        // TODO: remove comment
         for (int i = 0; i < numOfStrings; i++) {
             Generex randomGenerex = generexes.get(rand.nextInt(generexes.size()));
             String randomString = randomGenerex.random();
             randomInput.append(randomString);
         }
-
         return randomInput.toString();
+
+        // TODO: delete later (for checking regex
+        /*for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 35; j++) {
+                Generex randomGenerex = generexes.get(j);
+                String randomString = randomGenerex.random();
+                randomInput.append(randomString);
+                randomInput.append("\n");
+            }
+        }
+        // check comment type 2
+        for (int i = 0; i < 3; i++) {
+            Generex randomGenerex = generexes.get(4);
+            String randomString = randomGenerex.random();
+            randomInput.append(randomString);
+            randomInput.append("\n");
+        }*/
     }
 
     public static void deleteFolderContent(File folder) {
