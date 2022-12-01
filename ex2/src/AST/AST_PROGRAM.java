a83d31c44fb622fea0ed3062671c89f9fc05aece
Rotem Assa <rotem_a@optimove.com>
2022-12-01 14:30:40 +0200

    created AST_PROGRAM

--- ex2/src/AST/AST_PROGRAM.java
+++ ex2/src/AST/AST_PROGRAM.java
@@ -5,13 +5,13 @@ public class AST_STMT_LIST extends AST_Node
 	/****************/
 	/* DATA MEMBERS */
 	/****************/
-	public AST_STMT head;
-	public AST_STMT_LIST tail;
+	public AST_DEC head;
+	public AST_PROGRAM tail;
 
 	/******************/
 	/* CONSTRUCTOR(S) */
 	/******************/
-	public AST_STMT_LIST(AST_STMT head,AST_STMT_LIST tail)
+	public AST_PROGRAM(AST_DEC head,AST_PROGRAM tail)
 	{
 		/******************************/
 		/* SET A UNIQUE SERIAL NUMBER */
