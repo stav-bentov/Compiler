8615e2db0b747f0fecb43777df2a33e5a1d97c52
Rotem Assa <rotem_a@optimove.com>
2022-12-03 20:35:21 +0200

    made AST_VAR_DEC generic

--- ex2/src/AST/AST_VAR_DEC.java
+++ ex2/src/AST/AST_VAR_DEC.java
@@ -1,10 +1,9 @@
 package AST;
 
-public class AST_VAR_DEC extends AST_Node{
+public class AST_VAR_DEC<T extends AST_Node> extends AST_Node{
     public AST_TYPE type;
     public String name;
-    public AST_EXP exp;
-    public AST_NEWEXP newExp;
+    public T exp;
 
     public AST_VAR_DEC(AST_TYPE type, String name){
         this.type = type;
@@ -13,20 +12,11 @@ public class AST_VAR_DEC extends AST_Node{
         System.out.format("====================== varDec -> %s ID(%s)\n", type.type, name);
     }
 
-    public AST_VAR_DEC(AST_TYPE type, String name, AST_EXP exp){
+    public AST_VAR_DEC(AST_TYPE type, String name, T exp){
         this.type = type;
         this.name = name;
         this.exp = exp;
         SerialNumber = AST_Node_Serial_Number.getFresh();
         System.out.format("====================== varDec -> %s ID(%s) := exp\n", type.type, name);
-    }
-
-    public AST_VAR_DEC(AST_TYPE type, String name, AST_EXP exp, AST_NEWEXP newExp){
-        this.type = type;
-        this.name = name;
-        this.exp = exp;
-        this.newExp = newExp;
-        SerialNumber = AST_Node_Serial_Number.getFresh();
-        System.out.format("====================== varDec -> %s ID(%s) := newExp\n", type.type, name);
     }
 }
