2af3366eb6cb2daffa9661010a0741c51fa33658
Rotem Assa <rotem_a@optimove.com>
2022-12-03 19:29:47 +0200

    refactored type and vardec to use overloading

--- ex2/src/AST/AST_TYPE.java
+++ ex2/src/AST/AST_TYPE.java
@@ -7,6 +7,12 @@ public class AST_TYPE extends AST_Node{
         this.type = type;
         this.idValue = idValue;
         SerialNumber = AST_Node_Serial_Number.getFresh();
+        System.out.format("====================== type -> ID(%s)\n", idValue);
+    }
 
+    public AST_TYPE(String type){
+        this.type = type;
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+        System.out.format("====================== dec -> %s\n", type);
     }
 }
--- ex2/src/AST/AST_VARDEC.java
+++ ex2/src/AST/AST_VARDEC.java
@@ -6,6 +6,21 @@ public class AST_VARDEC extends AST_Node{
     public AST_EXP exp;
     public AST_NEWEXP newExp;
 
+    public AST_VARDEC(AST_TYPE type, String name){
+        this.type = type;
+        this.name = name;
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+        System.out.format("====================== varDec -> %s ID(%s)\n", type.type, name);
+    }
+
+    public AST_VARDEC(AST_TYPE type, String name, AST_EXP exp){
+        this.type = type;
+        this.name = name;
+        this.exp = exp;
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+        System.out.format("====================== varDec -> %s ID(%s) := exp\n", type.type, name);
+    }
+
     public AST_VARDEC(AST_TYPE type, String name, AST_EXP exp, AST_NEWEXP newExp){
         this.type = type;
         this.name = name;
