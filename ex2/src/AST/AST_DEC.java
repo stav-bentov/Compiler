7a1de9c54e3a542e48e4862aa0e4bcf9b81369a4
Rotem Assa <rotem_a@optimove.com>
2022-12-03 19:13:21 +0200

    refactored AST_DEC to use overloading

--- ex2/src/AST/AST_DEC.java
+++ ex2/src/AST/AST_DEC.java
@@ -7,12 +7,28 @@ public class AST_DEC extends AST_Node
 	public AST_CLASSDEC classdec;
 	public AST_ARRAYTYPEDEF arraytypedef;
 
-	public AST_DEC(AST_VARDEC vardec, AST_FUNCDEC funcdec, AST_CLASSDEC classdec, AST_ARRAYTYPEDEF arraytypedef){
+	public AST_DEC(AST_VARDEC vardec){
 		this.vardec = vardec;
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+		System.out.format("====================== dec -> varDec\n");
+	}
+
+	public AST_DEC(AST_FUNCDEC funcdec){
 		this.funcdec = funcdec;
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+		System.out.format("====================== dec -> funcDec\n");
+	}
+
+	public AST_DEC(AST_CLASSDEC classdec){
 		this.classdec = classdec;
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+		System.out.format("====================== dec -> classDec\n");
+	}
+
+	public AST_DEC(AST_ARRAYTYPEDEF arraytypedef){
 		this.arraytypedef = arraytypedef;
 		SerialNumber = AST_Node_Serial_Number.getFresh();
+		System.out.format("====================== dec -> arrayTypedef\n");
 	}
 
 }
