8f72448048f73a4d034e0d91b015201472349ff9
Rotem Assa <rotem_a@optimove.com>
2022-12-04 13:53:13 +0200

    added printme to AST_ARGUMENT

--- ex2/src/AST/AST_ARGUMENT.java
+++ ex2/src/AST/AST_ARGUMENT.java
@@ -8,6 +8,30 @@ public class AST_ARGUMENT extends AST_Node{
         this.type = type;
         this.name = name;
         SerialNumber = AST_Node_Serial_Number.getFresh();
+    }
 
+    public void PrintMe() {
+        /*********************************/
+        /* AST NODE TYPE = AST FIELD VAR */
+        /*********************************/
+        System.out.print("AST NODE ID VAR\n");
+
+        /**********************************************/
+        /* RECURSIVELY PRINT VAR, then FIELD NAME ... */
+        /**********************************************/
+        if(type != null) type.PrintMe();
+        System.out.format("ID( %s )\n",this.name);
+
+        /***************************************/
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /***************************************/
+        AST_GRAPHVIZ.getInstance().logNode(
+                SerialNumber,
+                String.format("ID\ntype\n...->%s",this.name));
+
+        /****************************************/
+        /* PRINT Edges to AST GRAPHVIZ DOT file */
+        /****************************************/
+        if (type != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,type.SerialNumber);
     }
 }
