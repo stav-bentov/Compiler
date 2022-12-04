0a467bccdebd3275317426b1eba2ad602aac6978
Rotem Assa <rotem_a@optimove.com>
2022-12-04 14:21:04 +0200

    added print AST_CFIELD_DEC

--- ex2/src/AST/AST_CFIELD_DEC.java
+++ ex2/src/AST/AST_CFIELD_DEC.java
@@ -27,4 +27,27 @@ public class AST_CFIELD_DEC<T extends AST_Node> extends AST_CFIELD{
         /*******************************/
         this.dec = dec;
     }
+
+    public void PrintMe() {
+        /*********************************/
+        /* AST NODE TYPE = AST FIELD VAR */
+        /*********************************/
+        System.out.print("AST_CFIELD_DEC\n");
+
+        /**********************************************/
+        /* RECURSIVELY PRINT VAR, then FIELD NAME ... */
+        /**********************************************/
+        if(dec != null) dec.PrintMe();
+        System.out.format("CFIELD_DEC\n");
+
+        /***************************************/
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /***************************************/
+        AST_GRAPHVIZ.getInstance().logNode(SerialNumber, "cfield_dec");
+
+        /****************************************/
+        /* PRINT Edges to AST GRAPHVIZ DOT file */
+        /****************************************/
+        if (dec != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, dec.SerialNumber);
+    }
 }
