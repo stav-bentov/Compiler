b31d9861fbd0fc13ec18406fd4338e8d3202d66b
Rotem Assa <rotem_a@optimove.com>
2022-12-06 13:34:04 +0200

    changed name to id in AST_ARGUMENT

--- ex2/src/AST/AST_ARGUMENT.java
+++ ex2/src/AST/AST_ARGUMENT.java
@@ -2,13 +2,13 @@ package AST;
 
 public class AST_ARGUMENT extends AST_Node{
     public AST_TYPE type;
-    public String name;
+    public String id;
 
-    public AST_ARGUMENT(AST_TYPE type, String name){
+    public AST_ARGUMENT(AST_TYPE type, String id){
         this.type = type;
-        this.name = name;
+        this.id = id;
         SerialNumber = AST_Node_Serial_Number.getFresh();
-        System.out.format("===================== argument -> type(%s) ID(%s)\n", type.type, this.name);
+        System.out.format("===================== argument -> type(%s) ID(%s)\n", type.type, this.id);
     }
 
     public void PrintMe() {
@@ -21,14 +21,14 @@ public class AST_ARGUMENT extends AST_Node{
         /* RECURSIVELY PRINT VAR, then FIELD NAME ... */
         /**********************************************/
         if(type != null) type.PrintMe();
-        System.out.format("ID( %s )\n",this.name);
+        System.out.format("ID( %s )\n",this.id);
 
         /***************************************/
         /* PRINT Node to AST GRAPHVIZ DOT file */
         /***************************************/
         AST_GRAPHVIZ.getInstance().logNode(
                 SerialNumber,
-                String.format("argument: %s",this.name));
+                String.format("argument: %s",this.id));
 
         /****************************************/
         /* PRINT Edges to AST GRAPHVIZ DOT file */
