8a8287ca086218dce531c360d26c66dfa7299b45
Rotem Assa <rotem_a@optimove.com>
2022-12-20 23:00:55 +0200

    SemantMe to AST_LIST

--- ex3/src/AST/AST_LIST.java
+++ ex3/src/AST/AST_LIST.java
@@ -39,4 +39,76 @@ public class AST_LIST<T extends AST_Node> extends AST_Node{
         AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, head.SerialNumber);
         if (tail != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, tail.SerialNumber);
     }
+
+    public TYPE SemantMe(){
+        TYPE head_type = head.SemantMe();
+
+        TYPE_LIST tail_type = null;
+        head_type.len = 1;
+
+        if (tail != null){
+            tail_type = tail.SemantMe();
+            head_type.len = tail_type.len + 1;
+        }
+
+        string name;
+        switch (head.type){
+            case "AST_DEC":
+                name = GetAstDecName(head);
+                break;
+
+            case "AST_ARGUMENT":
+                name = head.id;
+                break;
+
+            case "AST_CFIELD":
+                name = GetAstCfieldName(head);
+                break;
+
+            case "AST_STMT": //TODO: check if ok that is empty
+            case "AST_EXP": //TODO: check if ok that is empty
+        }
+
+        TYPE_LIST type_list = new TYPE_LIST(head_type, tail_type, name);
+
+        return head_type;
+    }
+
+    public String GetAstDecName(AST_DEC head){
+        String name;
+        switch (head.type){
+            case AST_ARRAY_TYPEDEF:
+                name = head.name;
+                break;
+
+            case AST_CLASS_DEC:
+                name = head.className;
+                break;
+
+            case AST_FUNC_DEC:
+                name = head.name;
+                break;
+
+            case AST_VAR_DEC:
+                name = head.id;
+                break;
+        }
+
+        return name;
+    }
+
+    public String GetAstCfieldName(AST_CFIELD head){
+        String name;
+        switch((AST_CFIELD_DEC head).type){
+            case "varDec":
+                name = (AST_VAR_DEC (AST_CFIELD_DEC head).dec).id;
+                break;
+
+            case "funcDec":
+                name = (AST_FUNC_DEC (AST_CFIELD_DEC head).dec).name;
+                break;
+        }
+
+        return name;
+    }
 }
