5f130bd06c6fa51a332581c5fd984c6571d7d34d
Rotem Assa <rotem_a@optimove.com>
2022-12-21 16:51:18 +0200

    removed count_fields from TYPE_CLASS

--- ex3/src/TYPES/TYPE_CLASS.java
+++ ex3/src/TYPES/TYPE_CLASS.java
@@ -13,7 +13,6 @@ public class TYPE_CLASS extends TYPE
 	/* packed together with the class methods         */
 	/**************************************************/
 	public TYPE_LIST data_members;
-	public int count_fields;
 	public String name;
 	
 	/****************/
