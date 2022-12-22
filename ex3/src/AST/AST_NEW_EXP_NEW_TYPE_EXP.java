d621d2af27b66acdf3b2eee7ccb89864817b9517
Lilach Daniel <lilachdaniel100@gmail.com>
2022-12-22 18:11:22 +0200

    Added SemanticException + added SemantMe to newExp case

--- ex3/src/AST/AST_NEW_EXP_NEW_TYPE_EXP.java
+++ ex3/src/AST/AST_NEW_EXP_NEW_TYPE_EXP.java
@@ -1,4 +1,5 @@
 package AST;
+import TYPES.*;
 
 public class AST_NEW_EXP_NEW_TYPE_EXP extends AST_NEW_EXP {
     public AST_TYPE type;
@@ -35,5 +36,61 @@ public class AST_NEW_EXP_NEW_TYPE_EXP extends AST_NEW_EXP {
         /****************************************/
         AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, type.SerialNumber);
         if (exp != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, exp.SerialNumber);
+    }
+
+    public TYPE SemantMe() throws SemanticException {
+        TYPE instanceType = type.SemantMe(); // This type exists in the symbol table
+
+        /* New instance of an array */
+        if (exp != null) {
+            return SemantMeCaseArray(instanceType);
+        }
+
+        /* New instance of a class */
+        else if (instanceType instanceof TYPE_CLASS) {
+            return instanceType;
+        }
+
+        else {
+            throw new SemanticException(
+                    "Cannot create instances of this type",
+                    this);
+        }
+    }
+
+    private TYPE SemantMeCaseArray(TYPE arrayMembersType) throws SemanticException {
+        /* Check type of array members */
+        if (!(arrayMembersType instanceof TYPE_CLASS) &&
+            !(arrayMembersType instanceof TYPE_ARRAY) &&
+            !(arrayMembersType instanceof TYPE_INT)   &&
+            !(arrayMembersType instanceof TYPE_STRING)) {
+            throw new SemanticException(
+                    "Array has an invalid memebres' type",
+                    this
+            );
+        }
+
+        /* Check expression */
+        TYPE expType = exp.SemantMe();
+
+        /* Check if exp is a constant */
+        if (exp instanceof AST_EXP_OPT) {
+            AST_EXP_OPT constExp = (AST_EXP_OPT) exp;
+
+            if (!(constExp.opt.equals("INT") && constExp.i > 0)) {
+                throw new SemanticException(
+                        "Allocating arrays with the new operator, when done with a constant, must be greater then zero",
+                        this);
+            }
+        }
+
+        /* Check if the expression evaluates to an integer */
+        if (!(expType instanceof TYPE_INT)) {
+            throw new SemanticException(
+                    "Allocating arrays with the new operator must be done with an integral size",
+                    this);
+        }
+
+        return new TYPE_ARRAY(arrayMembersType, null);
     }
 }
