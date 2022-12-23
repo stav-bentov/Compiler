0f80351b70ab8666ac7b91eb5892b367649bd13c
Rotem Assa <rotem_a@optimove.com>
2022-12-23 16:09:47 +0200

    removed isClass from TYPE_VAR

--- ex3/src/TYPES/TYPE_VAR.java
+++ ex3/src/TYPES/TYPE_VAR.java
@@ -3,17 +3,11 @@ package src.TYPES;
 import jdk.nashorn.internal.ir.Symbol;
 
 public class TYPE_VAR extends TYPE{
-    public String id;
     public TYPE type;
 
-    public TYPE_VAR(String id, TYPE type){
-        this.id = id;
+    public TYPE_VAR(String name, TYPE type){
+        this.name = id;
         this.type = type;
-    }
-
-    @Override
-    public boolean isClass(){
-        return SYMBOL_TABLE.getInstance().find(id).isClass();
     }
 
     @Override
