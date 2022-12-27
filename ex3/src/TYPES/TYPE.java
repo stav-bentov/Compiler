c9069a2fe83e684d5cabba869db385f3adc5b56f
stav-bentov <stavbentov5@gmail.com>
2022-12-27 10:17:02 +0200

    Fixed TYPE

--- ex3/src/TYPES/TYPE.java
+++ ex3/src/TYPES/TYPE.java
@@ -26,18 +26,29 @@ public abstract class TYPE
 		{
 			/* Only variables of arrays and classes can be defined with null expression */
 			if (this instanceof TYPE_CLASS || this instanceof TYPE_ARRAY)
+			{
 				return true;
+			}
 		}
 		else
 		{
 			if (this instanceof TYPE_CLASS && expType instanceof TYPE_CLASS)
 			{
 				if (((TYPE_CLASS) expType).inheritsFrom((TYPE_CLASS)this))
+				{
 					return true;
+				}
 			}
 			else
 			{
-				/* Cases of int/ string/ array*/
+				if (this instanceof TYPE_ARRAY)
+				{
+					if (((TYPE_ARRAY) this).arrayType == expType)
+					{
+						return true;
+					}
+				}
+				/* Cases of int/ string*/
 				if(this == expType)
 				{
 					return true;
