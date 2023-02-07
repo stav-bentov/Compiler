f44f5680687d37e1581c22dca5c2bcb305e32843
stav-bentov <stavbentov5@gmail.com>
2023-02-07 13:44:09 +0200

    added add_Strings

---
+++ ex4/src/IR/IRcommand_Binop_Add_Strings.java
@@ -0,0 +1,34 @@
+/***********/
+/* PACKAGE */
+/***********/
+package IR;
+
+/*******************/
+/* GENERAL IMPORTS */
+/*******************/
+
+/*******************/
+/* PROJECT IMPORTS */
+/*******************/
+
+import MIPS.MIPSGenerator;
+import TEMP.TEMP;
+
+public class IRcommand_Binop_Add_Strings extends IRcommand
+{
+	public TEMP t1;
+	public TEMP t2;
+	public TEMP dst;
+
+	public IRcommand_Binop_Add_Strings(TEMP dst, TEMP t1, TEMP t2)
+	{
+		this.dst = dst;
+		this.t1 = t1;
+		this.t2 = t2;
+	}
+
+	public void MIPSme()
+	{
+		MIPSGenerator.getInstance().add_strings(dst,t1,t2);
+	}
+}
