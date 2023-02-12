5953f0e3d7e2e216b78523bae1bfb243582b7e22
Rotem Assa <rotem_a@optimove.com>
2023-02-12 17:54:27 +0200

    fixed some bugs in init of FunctionIRList

--- ex4/src/IR/Analysis.java
+++ ex4/src/IR/Analysis.java
@@ -3,36 +3,36 @@ package src.IR;
 public class Analysis {
     public static FunctionIRList init(){
         IR.IRcommandList start = null;
-        IR.IRcommandList curr = IR.getInstance().tail;
+        IR.IRcommandList curr = IR.getInstance().head;//TODO: check how to get first command of all IR commands file
 
-        FunctionIRList ret = new FunctionIRList(null, null, 0);
+        FunctionIRList ret = new FunctionIRList(null, 0);
         FunctionIRList temp = ret;
 
         int len = 1;
         while(curr != null){
             //if we go into this if clause we know we found a label that starts a function
-            if((curr.head != null) && (curr.head instanceof IR.IRcommand_Label) && (IR.IRcommand_Label curr.head).label_type == labelEnum.FUNCSTART){//wtf does it want here? why error?
-                start = curr.tail;
+            if((curr.head instanceof IR.IRcommand_Label) && ((IR.IRcommand_Label curr.head).label_type == labelEnum.FUNCSTART)){//IRCommand instance of IRCommandStartFunc
+                start = curr;
                 len = 1;
             }
 
             //here we see a label that marks the end of a function
-            if((curr.head != null) && (curr.head instanceof IR.IRcommand_Label) && (IR.IRcommand_Label curr.head).label_type == labelEnum.FUNCEND){//wtf does it want here? why error?
+            if((curr.head instanceof IR.IRcommand_Label) && (IR.IRcommand_Label curr.head).label_type == labelEnum.FUNCEND){
                 //if we also saw a start to that function we create a function ir list object and move to the next one
-                if(start != null){
-                    temp.next = new FunctionIRList(start, len);
-                    start = null;
-                    len = 0;
-                    temp = temp.next;
-                }
+                temp = new FunctionIRList(start, len);
+                start = null;
+                len = 0;
+                temp = temp.next;
             }
 
             curr = curr.tail;
             len++;
         }
+
+        return ret;
     }
 
-    public static void AnalyzeMe(){
+    public static void AnalyzeMe() throws Exception{
         FunctionIRList function_IRcommands_list = init();
         while(function_IRcommands_list != null){
             function_IRcommands_list.AnalyzeMe();
