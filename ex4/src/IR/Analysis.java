a738330cf3cf493360c9608ca80061048e8de6fb
Rotem Assa <rotem_a@optimove.com>
2023-02-13 16:25:13 +0200

    fixed project structure

--- ex4/src/IR/Analysis.java
+++ ex4/src/IR/Analysis.java
@@ -1,23 +1,31 @@
-package src.IR;
+package IR;
+
+import IR.*;
 
 public class Analysis {
     public static FunctionIRList init(){
-        IR.IRcommandList start = null;
-        IR.IRcommandList curr = IR.getInstance().head;//TODO: check how to get first command of all IR commands file
+        IRcommandList start = null;
+        IRcommandList curr = IR.getInstance().tail;
+        IRcommand firstCmd = IR.getInstance().head;
 
         FunctionIRList ret = new FunctionIRList(null, 0);
         FunctionIRList temp = ret;
 
         int len = 1;
+        if((firstCmd instanceof IRcommand_Label)){//IRCommand instance of IRCommandStartFunc
+            start = curr;
+            len = 1;
+        }
+
         while(curr != null){
             //if we go into this if clause we know we found a label that starts a function
-            if((curr.head instanceof IR.IRcommand_Label) && ((IR.IRcommand_Label curr.head).label_type == labelEnum.FUNCSTART)){//IRCommand instance of IRCommandStartFunc
+            if((curr.head instanceof IRcommand_Label) && ((IRcommand_Label curr.head).label_type == labelEnum.FUNCSTART)){//IRCommand instance of IRCommandStartFunc
                 start = curr;
                 len = 1;
             }
 
             //here we see a label that marks the end of a function
-            if((curr.head instanceof IR.IRcommand_Label) && (IR.IRcommand_Label curr.head).label_type == labelEnum.FUNCEND){
+            if((curr.head instanceof IRcommand_Label) && (IRcommand_Label curr.head).label_type == labelEnum.FUNCEND){
                 //if we also saw a start to that function we create a function ir list object and move to the next one
                 temp = new FunctionIRList(start, len);
                 start = null;
@@ -36,6 +44,7 @@ public class Analysis {
         FunctionIRList function_IRcommands_list = init();
         while(function_IRcommands_list != null){
             function_IRcommands_list.AnalyzeMe();
+            function_IRcommands_list = function_IRcommands_list.next;
         }
     }
 }
--- ex4/src/IR/FunctionIRList.java
+++ ex4/src/IR/FunctionIRList.java
@@ -1,10 +1,11 @@
-package src.IR;
+package IR;
 
+import IR.*;
 import java.util.*;
 
 
 public class FunctionIRList {
-    public IR.IRcommandList start;
+    public IRcommandList start;
     public int len;
     public FunctionIRList next;
 
@@ -18,7 +19,7 @@ public class FunctionIRList {
     private HashMap<Integer, Integer> coloring;
     private final int num_of_registers = 10;
 
-    public FunctionIRList(IR.IRcommandList start, int len){
+    public FunctionIRList(IRcommandList start, int len){
         this.start = start;
         this.len = len;
     }
@@ -33,11 +34,11 @@ public class FunctionIRList {
 
     private void Create_CFG(){
         CFG_Node[] arr = new CFG_Node[len];
-        IR.IRcommandList curr = start;
+        IRcommandList curr = start;
 
         //first of all each one should point to the next cmd except for when next cmd in under a label
         for(int i = 0; i < len; i++){
-            if((curr.head != null) && !(curr.head instanceof IR.IRcommand_Label)){
+            if((curr.head != null) && !(curr.head instanceof IRcommand_Label)){
                 arr[i] = new CFG_Node(curr.head.serialNumber, curr.head);//need to under what is the SerialNumber (meaning, number of the TEMP, before coloring)
             }
             curr = curr.tail;
@@ -47,33 +48,33 @@ public class FunctionIRList {
         curr = start;
         for(int i = 0; i < len; i++){
             //if we finished traversing or we are on a label statement no need to treat this cmd, continue
-            if(curr.head == null || curr.head instanceof IR.IRcommand_Label){//should we really test curr.head == null? when will we have an empty IRCommand?
+            if(curr.head == null || curr.head instanceof IRcommand_Label){//should we really test curr.head == null? when will we have an empty IRCommand?
                 curr = curr.tail;
                 continue;
             }
 
             //if it's a not a jump then the current node points to the next
-            if(!(curr.head instanceof IR.IRcommand_Jump_Label)){
+            if(!(curr.head instanceof IRcommand_Jump_Label)){
                 if(i < len - 1){
                     arr[i].sons.add(arr[i + 1]);
                 }
             }
 
             //if it is some kind of jump label we would look for the label within the function. if the label is not here then we don't have to do anything.
-            if(curr.head instanceof IR.IRcommand_Jump_Label || curr.head instanceof IR.IRcommand_Jump_If_Eq_Zero){
+            if(curr.head instanceof IRcommand_Jump_Label || curr.head instanceof IRcommand_Jump_If_Eq_Zero){
                 boolean has_found_current_label_in_function = false;
                 for(int j = 0; j < len; j++){//think whether to change implementation: save all labels in hash-table so we don't need to iterate over all commands
 
                     //if we found a label check if the current label is the label referenced by the jump cmd at arr[i]
-                    if(arr[j].cmd instanceof IR.IRcommand_Label){
-                        if((arr[i].cmd instanceof IR.IRcommand_Jump_Label &&  ((IR.IRcommand_Jump_Label) arr[i].cmd).label_name.equals(((IR.IRcommand_Label)arr[j].cmd).label_name)) ||
-                            (arr[i].cmd instanceof IR.IRcommand_Jump_If_Eq_To_Zero &&  ((IR.IRcommand_Jump_If_Eq_To_Zero) arr[i].cmd).label_name.equals(((IR.IRcommand_Label)arr[j].cmd).label_name))){
+                    if(arr[j].cmd instanceof IRcommand_Label){
+                        if((arr[i].cmd instanceof IRcommand_Jump_Label &&  ((IRcommand_Jump_Label) arr[i].cmd).label_name.equals(((IRcommand_Label)arr[j].cmd).label_name)) ||
+                            (arr[i].cmd instanceof IRcommand_Jump_If_Eq_To_Zero &&  ((IRcommand_Jump_If_Eq_To_Zero) arr[i].cmd).label_name.equals(((IRcommand_Label)arr[j].cmd).label_name))){
                             has_found_current_label_in_function = true;
                         }
                     }
 
                     //if we found the label then arr[j] is the label command that arr[i] can jump to. we want to link arr[i[ to the command AFTER the label
-                    if(has_found_current_label_in_function && arr[j].cmd instanceof IR.IRcommand_Label){
+                    if(has_found_current_label_in_function && arr[j].cmd instanceof IRcommand_Label){
                         arr[i].sons.add(arr[j + 1]);
                     }
                 }
@@ -241,8 +242,8 @@ public class FunctionIRList {
 
     private void AssignTemps(){
         for(int i = 0; i < len; i++){
-            IR.IRcommand cmd = CFG[i].cmd.head;//TODO: fix
-            if(cmd != null && !(cmd instanceof IR.IRcommand_Label) && cmd.temps.size() > 0){//IRCommand needs to have a field temps - all the temps
+            IRcommand cmd = CFG[i].cmd.head;//TODO: fix
+            if(cmd != null && !(cmd instanceof IRcommand_Label) && cmd.temps.size() > 0){//IRCommand needs to have a field temps - all the temps
                 for(TEMP.TEMP temp : cmd.temps){
                     if (!coloring.containsKey(temp.getSerialNumber())){//TODO: understand why
                         temp.real = 0;
